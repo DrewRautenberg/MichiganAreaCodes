b4a58e6fc79504b3f59c96edf55d32df08972a05
Drew Rautenberg <drew@drewrautenberg.com>
2017-09-20 16:17:16 -0400

    is Michigan test

--- src/MichiganAreaCodes.java
+++ src/MichiganAreaCodes.java
@@ -1,9 +1,8 @@
-import javax.swing.*;
-
 /*Project: Michigan Area Codes
  *Author: Drew Rautenberg
  *Lab: Wed at 2:30
  */
+import javax.swing.*;
 public class MichiganAreaCodes {
     public static void main(String[] args) {
         //Variable Declaration
@@ -13,18 +12,58 @@ public class MichiganAreaCodes {
         int prefix;
         int suffix;
         int remainder;
+        boolean isMi;
 
         //User Input
         input = JOptionPane.showInputDialog("Enter the phone number.");
-        number = Long.valueOf(input);
+        number = Long.parseLong(input);
 
         //Spit Phone Number
-        areaCode = (int)number / 1000;
-        remainder
-        System.out.println(areaCode);
-        prefix =(int) (number % 1000) / 1000;
-        System.out.println(prefix);
-        suffix = (int) (number % 1000000);
-        System.out.println(suffix);
+        System.out.println(number);
+        areaCode = (int) (number / 10000000);
+        remainder = (int) (number % 10000000);
+        prefix = remainder / 10000;
+        remainder = remainder % 10000;
+        suffix = remainder;
+
+        System.out.println("(" + areaCode + ") " + prefix + " " + suffix);
+
+        switch (areaCode){
+            case 231:
+            case 248:
+            case 269:
+            case 313:
+            case 517:
+            case 586:
+            case 616:
+            case 734:
+            case 810:
+            case 906:
+            case 947:
+            case 989:
+                isMi = true;
+                break;
+            default:
+                isMi = false;
+                 break;
+        }
+        if (isMi){
+            switch (areaCode){
+                case 231:
+                    System.out.println("Northwestern L");
+                case 248:
+                case 269:
+                case 313:
+                case 517:
+                case 586:
+                case 616:
+                case 734:
+                case 810:
+                case 906:
+                case 947:
+                case 989:
+            }
+        }
+
     }
 }
